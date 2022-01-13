e174c583d9dfbec65001747be479d904156e0958
Pro-coder-69 <52014992+Pro-coder-69@users.noreply.github.com>
2022-01-13 09:25:28 -0500

    v1.3

--- app/src/main/java/com/example/sqliteoperations/uploadItem.java
+++ app/src/main/java/com/example/sqliteoperations/uploadItem.java
@@ -2,7 +2,9 @@ package com.example.sqliteoperations;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class uploadItem extends AppCompatActivity {
 
@@ -10,5 +12,12 @@ public class uploadItem extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_upload_item);
+    }
+
+
+
+    public void accountPageActivity(View view) {
+        Intent intent = new Intent(this, accountPageActivity.class);
+        startActivity(intent);
     }
 }
